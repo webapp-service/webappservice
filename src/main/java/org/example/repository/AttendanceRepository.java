bc2ffc34a4efbc748291e0c34f73e832cd552aa6
Erika <erikac.ec80@gmail.com>
2023-05-13 14:20:43 -0300

    creando metodos CRUD attendance

--- src/main/java/org/example/repository/AttendanceRepository.java
+++ src/main/java/org/example/repository/AttendanceRepository.java
@@ -4,6 +4,9 @@ import org.example.entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
+
 }
