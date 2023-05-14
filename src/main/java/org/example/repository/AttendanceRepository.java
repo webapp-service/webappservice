f97756bb38b8dd3d226ac41c0ae0e386e45542a4
augustoba <94504327+augustoba@users.noreply.github.com>
2023-05-13 22:43:55 -0300

    Revert "Feature martin tsk#5"

--- src/main/java/org/example/repository/AttendanceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Attendance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
-}
